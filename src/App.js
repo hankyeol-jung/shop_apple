2de36f5342654c5d8fd8742816b75f475b6be694
hankyeol-jung <whd6023@gmail.com>
2022-10-31 20:48:16 +0900

    이미지 넣는 법 & public 폴더 이용하기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Button, Navbar, Container, Nav, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+      <Container>
+        <Row>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width={"80%"}
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width={"80%"}
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col md={4}>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width={"80%"}
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
