f1c16a7d1d4bf04d554088223db4c8de4707abdc
hankyeol-jung <whd6023@gmail.com>
2022-11-04 22:04:00 +0900

    리액트에서 서버와 통신하려면 ajax1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import data from "./data.js";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./routes/Detail";
+import axios from "axios";
 
 function App() {
   let [shoes, setShoes] = useState(data);
@@ -60,6 +61,21 @@ function App() {
                   })}
                 </Row>
               </Container>
+              <button
+                onClick={() => {
+                  axios
+                    .get("https://codingapple1.github.io/shop/data2.json")
+                    .then((result) => {
+                      let copy = [...shoes, ...result.data];
+                      setShoes(copy);
+                    })
+                    .catch(() => {
+                      console.log("실패");
+                    });
+                }}
+              >
+                버튼
+              </button>
             </>
           }
         />
