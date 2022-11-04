0f185e7714bc45f58228551399812a3d5baee9f3
hankyeol-jung <whd6023@gmail.com>
2022-11-04 21:30:47 +0900

    Lifecycle과 useEffect 2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,22 +2,27 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail(props) {
-  let [none, setNone] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setNone(false);
-    }, 2000);
-  });
-
+  let [pop, setPop] = useState(true);
   let [count, setCount] = useState(0);
-
   let { id, setId } = useParams();
+  let [num, setNum] = useState("");
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setPop(false);
+    }, 2000);
+    if (isNaN(num) == true) {
+      alert("그러지마세요");
+    }
+  }, [num]);
+
   let findPro = props.shoes.find(function (x) {
     return x.id == id;
   });
   return (
     <div className="container">
-      {none == true ? <Modal /> : null}
+      {pop == true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
       {count}
       <button
         onClick={() => {
@@ -38,6 +43,12 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
+          <input
+            type="text"
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
           <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}원</p>
@@ -46,10 +57,6 @@ function Detail(props) {
       </div>
     </div>
   );
-}
-
-function Modal() {
-  return <div className="alert alert-warning">2초이내 구매시 할인</div>;
 }
 
 export default Detail;
