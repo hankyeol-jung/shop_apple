fb1a17a1f6e66198df53d30067d135b7d970be4d
hankyeol-jung <whd6023@gmail.com>
2022-11-05 12:05:34 +0900

    리액트에서 탭 UI 만들기

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 function Detail(props) {
   let [pop, setPop] = useState(true);
   let [count, setCount] = useState(0);
   let { id, setId } = useParams();
   let [num, setNum] = useState("");
+  let [tab, setTab] = useState(0);
+
   useEffect(() => {
     let a = setTimeout(() => {
       setPop(false);
@@ -55,8 +58,53 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(0);
+            }}
+            eventKey="link0"
+          >
+            버튼0
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(1);
+            }}
+            eventKey="link1"
+          >
+            버튼1
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => {
+              setTab(2);
+            }}
+            eventKey="link2"
+          >
+            버튼2
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <TabContent tab={tab} />
     </div>
   );
+}
+
+function TabContent({ tab }) {
+  // if (tab == 0) {
+  //   return <div>내용0</div>;
+  // } else if (tab == 1) {
+  //   return <div>내용1</div>;
+  // } else if (tab == 2) {
+  //   return <div>내용2</div>;
+  // }
+  return [<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab];
 }
 
 export default Detail;
